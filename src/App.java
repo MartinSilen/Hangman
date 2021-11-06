8268961e30fa260372ac3eb3c16f5e2832227301
MartinSilen <iokiokna@gmail.com>
2021-11-06 15:10:57 +0200

    now you can fail and set difficulty

--- src/App.java
+++ src/App.java
@@ -4,16 +4,19 @@ public class App {
     public static void main(String[] args) {
         String key = WordStorage.getWord();
         Scanner reader = new Scanner(System.in);
-
+        System.out.println("Please set the possible number of mistakes:");
+        int tries = reader.nextInt();
+        reader.nextLine();
         System.out.println("I guessed a word!");
         char curGuess;
         char[] display = WordStorage.curConstruct(key).toCharArray();
         System.out.println(display);
 
-        int counter = 1;
 
-        while(Checker.winCheck(display, key) && counter < 21){
-            System.out.println("Current turn: " + counter + " out of 20");
+
+
+        while(Checker.winCheck(display, key) && Checker.counter <= tries){
+            System.out.println("Current number of mistakes: " + Checker.counter + " out of " + tries);
             System.out.println("Print a Letter to guess:");
             curGuess = reader.next().charAt(0);
             display  = Checker.Check(display, key, curGuess);
@@ -21,8 +24,9 @@ public class App {
 
             System.out.println(display);
 
-            counter++;
+
         }
+        System.out.println("You failed!");
 
     }
 }
--- src/Checker.java
+++ src/Checker.java
@@ -1,12 +1,18 @@
-public class Checker {
-    public static char[] Check(char[] current, String key, char guess) {
 
+
+public class Checker {
+    public static int counter = 0;
+    public static char[] Check(char[] current, String key, char guess) {
+    boolean changed = false;
 
 
         for (int i = 0; i < key.length(); i++) {
-            if (key.charAt(i) == guess) {current [i] = key.charAt(i);}
-
+            if (key.charAt(i) == guess) {
+                current [i] = key.charAt(i);
+                changed = true;
+            }
         }
+        if (!changed) {counter++;}
         return current;
     }
 
